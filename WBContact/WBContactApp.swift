414b1a0cd47563dd9896ed93b26be80ba3ae0f36
Samir Akhmadi <samirakhmadi@gmail.com>
2024-06-08 22:19:23 -0700

    Initial Commit

---
+++ WBContact/WBContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBContactApp.swift
+//  WBContact
+//
+//  Created by Samir on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
